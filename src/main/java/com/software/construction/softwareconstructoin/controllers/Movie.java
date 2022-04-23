b9728fa384eadd9381b57d0db130a23f702e3d19
Ahmadnaufal2525 <ahmadnaufal588@gmail.com>
2022-04-23 15:03:41 +0700

    menambahkan class movie dan implementasi swagger

---
+++ src/main/java/com/software/construction/softwareconstructoin/controllers/Movie.java
@@ -0,0 +1,12 @@
+package com.software.construction.softwareconstructoin.controllers;
+import java.util.list;
+public class Movie {
+    public String title;
+    public String Director;
+    public list<String> Stars;
+    public String Description;
+    public void Movie(){
+        
+    }
+}
+
